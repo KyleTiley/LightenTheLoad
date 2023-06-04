c94e6fb3e08b39f2d75aa8933d57cd5814617278
Kyle Tiley <kylestiley@gmail.com>
2023-06-04 16:21:04 +0200

    Some set up for UI

--- Game/GameUI/GameUI.gd
+++ Game/GameUI/GameUI.gd
@@ -1,8 +1,10 @@
 extends Control
 
+@onready var textLabel = $Info/TextEdit
+
+
 func _ready():
 	Zone.displayInfo.connect(_on_zones_display_info)
 
 func _on_zones_display_info(_name, _electricityUsed, _currentHappiness):
-	print(_name + " has used " + str(_electricityUsed) + " electricity")
-	print(_currentHappiness)
+	textLabel.text = _name + " has used " + str(_electricityUsed) + " electricity" + '\n' + "and is at " + str(_currentHappiness) + " happiness"
--- Game/Zones/zone.gd
+++ Game/Zones/zone.gd
@@ -2,16 +2,18 @@ extends Node
 
 # General Variables
 var timerCounter = 0
+var selectedZone = "none"
 
 # Electricity Variables
 var isUsingElectricity = true
+var allocatedElectricity = 10000
 var electricityUsage = 0
 var electricityUsed = 0
 
 # Happiness Variables
 var isHappy = true
 var maxHappiness
-var currentHappiness = 0
+var currentHappiness = 50	# Start at the middle happy point
 
 # Signals
 # Sends data to UI to display
@@ -25,7 +27,8 @@ func _physics_process(delta):
 	timerCounter += delta
 
 func _process(delta):
-	if timerCounter > 10:
+	# The game updates parameters every second
+	if timerCounter > 1:
 		UseElectricity()
 		HappinessController()
 		timerCounter = 0
@@ -34,6 +37,9 @@ func _process(delta):
 func UseElectricity():
 	if isUsingElectricity:
 		electricityUsed += electricityUsage
+		allocatedElectricity -= electricityUsage
+	else:
+		isHappy = false	# If a zone has no power it will be unhappy
 
 # Happiness Functions
 func HappinessController():
@@ -41,7 +47,10 @@ func HappinessController():
 		currentHappiness += 10
 	else:
 		currentHappiness -= 10
-	currentHappiness = clamp(currentHappiness, 0, 100)
+	currentHappiness = clamp(currentHappiness, 0, 100)	# Keeps happiness between 0 and 100
+	# If a zone hits 0 happiness the game is over
+	if currentHappiness <= 0:
+		print("GAME OVER")
 
 # Click methods
 func _on_area_3d_input_event(camera, event, position, normal, shape_idx):
@@ -49,4 +58,5 @@ func _on_area_3d_input_event(camera, event, position, normal, shape_idx):
 		# If the event is left mouse click
 		if event.pressed and event.button_index == 1:
 			# Have to use Zone declaration here otherwise subclasses cannot emit signal for some dumb fucking reason
+			selectedZone = name
 			Zone.displayInfo.emit(name, electricityUsed, currentHappiness)
