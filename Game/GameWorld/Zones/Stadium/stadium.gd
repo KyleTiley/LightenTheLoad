a64973a537c1aca0648e91763f615abb0d079776
Kyle Tiley <kylestiley@gmail.com>
2023-07-02 13:42:39 +0200

    Schedule fixes

--- Game/GameWorld/Zones/Stadium/stadium.gd
+++ Game/GameWorld/Zones/Stadium/stadium.gd
@@ -1,13 +1,17 @@
 extends "res://Game/GameWorld/Zones/zone.gd"
 
+var base_happiness_gain = 10
+var base_happiness_loss = 10
+
 func _ready():
 	electricity_usage = 5
-	happiness_gain = 10
-	happiness_loss = 10
+	happiness_gain = base_happiness_gain
+	happiness_loss = base_happiness_loss
 
 func _on_game_game_timer():
 	run_controllers()
 
+# Sets variables for the big game event
 func _on_big_game_start_big_game():
 	electricity_usage = 50
 	happiness_loss = 20
@@ -16,10 +20,10 @@ func _on_clock_loadshedding_time(_clock_hours, _clock_minutes):
 	if _clock_minutes == 0:
 		if _clock_hours == int(Global.stadium_schedule1) or _clock_hours == int(Global.stadium_schedule2):
 			print("loadshedding started at: " + name)
-			happiness_gain = 20
-			happiness_loss = 10
+			happiness_gain = base_happiness_gain * 2
+			happiness_loss = base_happiness_loss / 2
 	elif _clock_minutes == 30:
 		if _clock_hours == (int(Global.stadium_schedule1) + 2) or _clock_hours == (int(Global.stadium_schedule2) + 2):
 			print("loadshedding ended at: " + name)
-			happiness_loss = 20
-			happiness_gain = 10
+			happiness_gain = base_happiness_gain
+			happiness_loss = base_happiness_loss
--- Game/GameWorld/Zones/Township/township.gd
+++ Game/GameWorld/Zones/Township/township.gd
@@ -1,9 +1,12 @@
 extends "res://Game/GameWorld/Zones/zone.gd"
 
+var base_happiness_gain = 2
+var base_happiness_loss = 2
+
 func _ready():
 	electricity_usage = 10
-	happiness_gain = 2
-	happiness_loss = 2
+	happiness_gain = base_happiness_gain
+	happiness_loss = base_happiness_loss
 
 func _on_game_game_timer():
 	run_controllers()
@@ -12,10 +15,10 @@ func _on_clock_loadshedding_time(_clock_hours, _clock_minutes):
 	if _clock_minutes == 0:
 		if _clock_hours == int(Global.township_schedule1) or _clock_hours == int(Global.township_schedule2):
 			print("loadshedding started at: " + name)
-			happiness_gain = 4
-			happiness_loss = 2
+			happiness_gain = base_happiness_gain * 2
+			happiness_loss = base_happiness_loss / 2
 	elif _clock_minutes == 30:
 		if _clock_hours == (int(Global.township_schedule1) + 2) or _clock_hours == (int(Global.township_schedule2) + 2):
 			print("loadshedding ended at: " + name)
-			happiness_loss = 4
-			happiness_gain = 2
+			happiness_gain = base_happiness_gain
+			happiness_loss = base_happiness_loss
