2a020ea5a48c7bc5a4285515d02d91c4c615e38e
ViperWR <roetsjwr@gmail.com>
2023-11-29 17:35:42 +0200

    Particles Emojis Working

--- Game/Particles_Emojis.gd
+++ Game/Particles_Emojis.gd
@@ -10,6 +10,11 @@ extends Node3D
 @onready var _city_happy = $City2
 @onready var _stadium_happy = $Stadium2
 
+@onready var hap_suburb = $"../GameWorld/Zones/Suburb"
+@onready var hap_city = $"../GameWorld/Zones/City"
+@onready var hap_stadium = $"../GameWorld/Zones/Stadium"
+@onready var hap_town = $"../GameWorld/Zones/Township"
+
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
@@ -23,17 +28,47 @@ func _process(delta):
 	Town()
 
 func Suburb():
-	_suburb_angry.emitting = false
-	_suburb_happy.emitting = true
+	if hap_suburb.current_happiness > 80:
+		_suburb_angry.emitting = false
+		_suburb_happy.emitting = true
+	elif hap_suburb.current_happiness < 20:
+		_suburb_angry.emitting = true
+		_suburb_happy.emitting = false
+	else:
+		_suburb_angry.emitting = false
+		_suburb_happy.emitting = false
 
 func Stadium():
-	_stadium_angry.emitting = false
-	_stadium_happy.emitting = true
+	if hap_stadium.current_happiness > 80:
+		_stadium_angry.emitting = false
+		_stadium_happy.emitting = true
+	elif hap_stadium.current_happiness < 20:
+		_stadium_angry.emitting = true
+		_stadium_happy.emitting = false
+	else:
+		_stadium_angry.emitting = false
+		_stadium_happy.emitting = false
+		
 
 func City():
-	_city_angry.emitting = true
-	_city_happy.emitting = false
+	if hap_city.current_happiness > 80:
+		_city_angry.emitting = false
+		_city_happy.emitting = true
+	elif hap_city.current_happiness < 20:
+		_city_angry.emitting = true
+		_city_happy.emitting = false
+	else:
+		_city_angry.emitting = false
+		_city_happy.emitting = false
+
 
 func Town():
-	_town_angry.emitting = true
-	_town_happy.emitting = false
+	if hap_town.current_happiness > 80:
+		_town_angry.emitting = false
+		_town_happy.emitting = true
+	elif hap_town.current_happiness < 20:
+		_town_angry.emitting = true
+		_town_happy.emitting = false
+	else:
+		_town_angry.emitting = false
+		_town_happy.emitting = false
